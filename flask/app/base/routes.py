2e48ddc606fafd39e3f0d1456ed31cdd45d00e35
ParkSeonIk <parksimis@gmail.com>
2021-05-17 22:10:41 +0900

    Init : Init flask

---
+++ flask/app/base/routes.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from flask import jsonify, render_template, redirect, request, url_for
+
+from app import db, login_manager
+from app.base import blueprint
+from app.base.forms import LoginForm, CreateAccountForm
+from app.base.models import User
+
+from app.base.util import verify_pass
+
+@blueprint.route('/')
+def route_default():
+    return redirect(url_for('home_blueprint.index'))
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return render_template('page-403.html'), 403
+
+@blueprint.errorhandler(403)
+def access_forbidden(error):
+    return render_template('page-403.html'), 403
+
+@blueprint.errorhandler(404)
+def not_found_error(error):
+    return render_template('page-404.html'), 404
+
+@blueprint.errorhandler(500)
+def internal_error(error):
+    return render_template('page-500.html'), 500
