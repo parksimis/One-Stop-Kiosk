13362540114e92bbe99abcc3a560bf8b910eb0a6
ParkSeonIk <parksimis@gmail.com>
2021-05-18 13:43:53 +0900

    Feat : Add video streaming 비디오 스트리밍 기능 구현

--- flask/app/base/routes.py
+++ flask/app/base/routes.py
@@ -12,22 +12,31 @@ from app.base.models import User
 
 from app.base.util import verify_pass
 
+
 @blueprint.route('/')
 def route_default():
     return redirect(url_for('home_blueprint.index'))
 
-@login_manager.unauthorized_handler
+# login_manager.unauthorized_handler
+
+
 def unauthorized_handler():
     return render_template('page-403.html'), 403
+
 
 @blueprint.errorhandler(403)
 def access_forbidden(error):
     return render_template('page-403.html'), 403
 
+
 @blueprint.errorhandler(404)
 def not_found_error(error):
     return render_template('page-404.html'), 404
 
+
 @blueprint.errorhandler(500)
 def internal_error(error):
     return render_template('page-500.html'), 500
+
+
+
--- flask/app/home/routes.py
+++ flask/app/home/routes.py
@@ -3,36 +3,57 @@ from flask import render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
+import cv2
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @blueprint.route('/index')
 def index():
-
     return render_template('index.html', segment='index')
+
+
+@blueprint.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @blueprint.route('/<template>')
 def route_template(template):
-
     try:
-        if not template.endswith( '.html' ):
+        if not template.endswith('.html'):
             template += '.html'
 
         # Detect the current page
-        segment = get_segment( request )
+        segment = get_segment(request)
 
         # Serve the file (if exists) from app/templates/FILE.html
-        return render_template( template, segment=segment )
+        return render_template(template, segment=segment)
 
     except TemplateNotFound:
         return render_template('page-404.html'), 404
-    
+
     except:
         return render_template('page-500.html'), 500
 
-# Helper - Extract current page name from request 
-def get_segment( request ): 
 
+# Helper - Extract current page name from request
+def get_segment(request):
     try:
 
         segment = request.path.split('/')[-1]
@@ -40,7 +61,7 @@ def get_segment( request ):
         if segment == '':
             segment = 'index'
 
-        return segment    
+        return segment
 
     except:
         return None  
